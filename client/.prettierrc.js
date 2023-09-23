0118aca7dd5b627ddb9de637c8b9564897314655
Valentin Olar <vali.olare@gmail.com>
2023-09-23 14:18:10 -0400

    Setup client with mui

---
+++ client/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    proseWrap: "preserve",
+    endOfLine: "auto"
+}
